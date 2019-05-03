4d93c811c75bfd58d3b551c10a8be63c4f19db02
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-03 21:19:05 +0900

    admin migration実行

--- db/migrate/20190503081609_devise_create_admins.rb
+++ db/migrate/20190503081609_devise_create_admins.rb
@@ -15,11 +15,11 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -31,7 +31,11 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      t.string :family_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :given_name, null: false
+      t.string :given_name_kana, null: false
+      t.integer :locale_id
 
       t.timestamps null: false
     end
