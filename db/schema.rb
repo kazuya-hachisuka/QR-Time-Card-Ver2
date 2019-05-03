5290384bbf8799b64cc554117d98248789f4f5c4
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-03 22:28:55 +0900

    add Manager model migrate OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_081609) do
+ActiveRecord::Schema.define(version: 2019_05_03_132225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,35 @@ ActiveRecord::Schema.define(version: 2019_05_03_081609) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "family_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "given_name", null: false
+    t.string "given_name_kana", null: false
+    t.integer "locale", null: false
+    t.integer "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_managers_on_admin_id"
+    t.index ["email"], name: "index_managers_on_email", unique: true
+    t.index ["family_name"], name: "index_managers_on_family_name"
+    t.index ["family_name_kana"], name: "index_managers_on_family_name_kana"
+    t.index ["given_name"], name: "index_managers_on_given_name"
+    t.index ["given_name_kana"], name: "index_managers_on_given_name_kana"
+    t.index ["locale"], name: "index_managers_on_locale"
+    t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
   end
 
 end
