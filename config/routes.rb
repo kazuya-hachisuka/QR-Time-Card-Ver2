6219cf5ecdb44b97fd594999a8e70a4569314ca3
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-05 16:00:11 +0900

    admins_controller modified sign_up可能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :managers
   devise_for :admins, controllers: {
+    registrations: 'admins/registrations',
     sessions: 'admins/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
