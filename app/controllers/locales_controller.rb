9eb2bbc095cbd4842d8f17db9cf9417209b980c0
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-05 18:03:03 +0900

    lacale modelにadmin_idを追加

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,2 +1,15 @@
 class LocalesController < ApplicationController
+  def new
+    @locale = Locale.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+
+  def locale_params
+    params.require(:locale).permit(:locale_name)
+  end
 end
--- db/migrate/20190503133146_create_locales.rb
+++ db/migrate/20190503133146_create_locales.rb
@@ -2,6 +2,7 @@ class CreateLocales < ActiveRecord::Migration[5.2]
   def change
     create_table :locales do |t|
       t.string :locale_name, null: false, unique: true, index: true
+      t.integer :admin_id, null: false, index: true
       t.timestamps
     end
   end
