ad3866aecfa8af3aeb130ed5bf4dd32f488b2774
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-06 17:45:14 +0900

    locale マイグレーションファイル変更

--- test/models/locale_test.rb
+++ test/models/locale_test.rb
@@ -2,16 +2,20 @@
 #
 # Table name: locales
 #
-#  id          :bigint           not null, primary key
-#  locale_name :string           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  admin_id    :integer          not null
+#  id              :bigint           not null, primary key
+#  control_number  :string(191)      not null
+#  locale_name     :string(191)      not null
+#  password_digest :string(191)      not null
+#  remember_token  :string(191)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  admin_id        :integer          not null
 #
 # Indexes
 #
-#  index_locales_on_admin_id     (admin_id)
-#  index_locales_on_locale_name  (locale_name)
+#  index_locales_on_admin_id        (admin_id)
+#  index_locales_on_control_number  (control_number)
+#  index_locales_on_locale_name     (locale_name)
 #
 
 require 'test_helper'
