70de6a8fd42d95a523bd1a1759cee7617ea4499f
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-06 18:32:36 +0900

    session_controller 変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,35 @@
 class SessionsController < ApplicationController
+  before_action :set_user, only:[:create]
+
+  def new
+  end
+
+  def create
+    if @locale.authenticate(session_params[:password])
+      sign_in(@locale)
+      redirect_to root_path
+    else
+      flash.now[:danger] = t('.flash.invaled_password')
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to login_path
+  end
+
+  private
+
+  def set_locale
+    @locale = Locale.find_by!(control_number: session_params[:control_number])
+  rescue
+    flash.now[:danger] = t('.flash.invalid_control_number')
+    render action: 'new'
+  end
+
+  #許可するパラメータ
+  def session_params
+    params.require(:session).permit(:control_number,:password)
+  end
 end
