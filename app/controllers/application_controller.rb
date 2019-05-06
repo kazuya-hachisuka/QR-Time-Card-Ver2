47ae6c725eae887ee4f543bfa3846f292aa68e2c
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-06 18:49:55 +0900

    application_controller 変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def sign_in(locale)
+    remenber_token = Locale.new_remember_token
+    cookies.permanent[:locale_remember] = new_remember_token
+    locale.update!(remember_token: Locale.encrypt(remenber_token))
+    @current_locale = locale
+  end
+
   protected
 
   def configure_permitted_parameters
