cf1bf145bf4c5e59ee9b792ec2936e412a312f9c
kazuya-hachisuka <kzy8sk@gmail.com>
2019-05-06 23:04:18 +0900

    adminのログアウト後、サインアップ後のリダイレクトを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,50 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :current_locale
-  before_action :require_sign_in!
-  helper_method :sign_in?
+  before_action :authenticate_admin!
+  # before_action :current_locale
+  # before_action :require_sign_in!
+  # helper_method :sign_in?
 
 
-  def current_locale
-    remember_token = Locale.encrypt(cookies[:locale_remember_token])
-    @current_locale ||= Locale.find_by(remember_token: remember_token)
-  end
+  # def current_locale
+  #   remember_token = Locale.encrypt(cookies[:locale_remember_token])
+  #   @current_locale ||= Locale.find_by(remember_token: remember_token)
+  # end
 
-  def sign_in(locale)
-    remenber_token = Locale.new_remember_token
-    cookies.permanent[:locale_remember] = new_remember_token
-    locale.update!(remember_token: Locale.encrypt(remenber_token))
-    @current_locale = locale
-  end
+  # def sign_in(locale)
+  #   remenber_token = Locale.new_remember_token
+  #   cookies.permanent[:locale_remember] = new_remember_token
+  #   locale.update!(remember_token: Locale.encrypt(remenber_token))
+  #   @current_locale = locale
+  # end
 
-  def sign_out
-    @current_locale = nil
-    cookies.delete(:locale_remember_token)
-  end
+  # def sign_out
+  #   @current_locale = nil
+  #   cookies.delete(:locale_remember_token)
+  # end
 
-  def signd_in?
-    @current_locale.present?
-  end
+  # def signd_in?
+  #   @current_locale.present?
+  # end
 
   private
 
-  def require_sign_in!
-    redirect_to login_path unless signed_in?
+  def after_sign_in_path_for(resource)
+    if admin_signed_in?
+      root_path
+    else
+      new_admin_session_path
+    end
   end
+
+  def after_sing_out_path_for(resource)
+    new_admin_session_path
+  end
+
+  # def require_sign_in!
+  #   redirect_to login_path unless signed_in?
+  # end
 
   protected
 
