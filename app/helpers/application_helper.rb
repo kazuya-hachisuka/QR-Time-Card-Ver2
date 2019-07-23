9d5db44e3b49e5e0f6a8b23ef126c5938640532f
kazuya-hachisuka <kzy8sk@gmail.com>
2019-07-24 00:03:01 +0900

    lcale showにて出退勤のすべての表示機能実装 helper作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  # 退勤時間 - 出勤時間 - 休憩時間合計= "%dh%02dm"に変換
+  def working_hours(work)
+    work_breaks = WorkBreak.where(work_id: work.id)
+    total_sec = 0
+    work_breaks.each do |work_break|
+      total_sec += work_break.out - work_break.in
+    end
+    sec = work.out - work.in - total_sec
+    work_min = sec.to_i / 60
+    hh, mm = work_min.divmod(60)
+    "%dh%02dm" % [hh,mm]
+  end
+
+  # work_id紐ついたwork_breakの全部をeachで　out - in =　"%dh%02dm"に変換
+  def total_work_break(work)
+    work_breaks = WorkBreak.where(work_id: work.id)
+    total_sec = 0
+    work_breaks.each do |work_break|
+      total_sec += work_break.out - work_break.in
+    end
+    work_break_min = total_sec.to_i / 60
+    hh, mm = work_break_min.divmod(60)
+    "%dh%02dm" % [hh,mm]
+  end
+
 end
